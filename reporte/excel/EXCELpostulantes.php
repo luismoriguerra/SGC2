4b0062a91cd4e5cd73b5c03734cc4d9cd6bca5ca
shevchenk <jorgeshevchenk@gmail.com>
2018-01-31 14:31:52 -0500

    act report

--- reporte/excel/EXCELpostulantes.php
+++ reporte/excel/EXCELpostulantes.php
@@ -1,11 +1,11 @@
 <?php
 /*conexion*/
 set_time_limit(3000);
-ini_set('memory_limit','3072M');
+ini_set('memory_limit','1024M');
 
 //
 error_reporting(E_ALL);
-ini_set("display_errors", 1);
+ini_set("display_errors", true);
 
 
 require_once '../../conexion/MySqlConexion.php';
@@ -329,9 +329,13 @@ $objPHPExcel->setActiveSheetIndex(0);
 
 // Redirect output to a client's web browser (Excel2007)
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-header('Content-Disposition: attachment;filename="SGC.ReportePostulantes.'.date("Y-m-d_His").'.xlsx"');
+header('Content-Disposition: attachment;filename="SGC_ReportePostulantes_'.date("Y_m_d_His").'.xlsx"');
 header('Cache-Control: max-age=0');
-
+// If you're serving to IE over SSL, then the following may be needed
+header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+header ('Pragma: public'); // HTTP/1.0
 $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
 $objWriter->save('php://output');
 exit;
